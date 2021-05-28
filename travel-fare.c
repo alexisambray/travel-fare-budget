22dfa50f7bae820886e737756335d2da9e52d3ef
Alexis Ambray <alexisambray@gmail.com>
2021-05-28 19:37:01 +0800

    Implemented getNumTravellers and getDistance

--- travel-fare.c
+++ travel-fare.c
@@ -2,6 +2,26 @@
 
 typedef enum { JEEPNEY = 1, BUS, TAXI, UBER } ModeTransport;
 
+int getNumTravellers(void) {
+  int numTravellers;
+
+  printf("Enter Number of People: ");
+  scanf("%d", &numTravellers);
+
+  return numTravellers;
+}
+
+int getDistance(void) {
+  int distance;
+
+  printf("Enter Distance to Travel: ");
+  scanf("%d", &distance);
+
+  return distance;
+}
+
 int main() {
+  int numTravellers = getNumTravellers();
+  int distance = getDistance();
   return 0;
 }
